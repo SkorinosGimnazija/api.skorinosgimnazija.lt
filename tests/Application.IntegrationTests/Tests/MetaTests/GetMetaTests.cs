021deb768d849edaf8a6b58c521308785170b859
eXmaS <76765030+eXmaS@users.noreply.github.com>
2022-05-22 13:39:14 +0300

    locale test

--- tests/Application.IntegrationTests/Tests/MetaTests/GetMetaTests.cs
+++ tests/Application.IntegrationTests/Tests/MetaTests/GetMetaTests.cs
@@ -85,6 +85,7 @@ public class GetMetaTests
             Slug = "slug1",
             LanguageId = 1,
             IsPublished = true,
+            ShowInFeed = true,
             PublishedAt = DateTime.UtcNow
         };
 
@@ -96,6 +97,7 @@ public class GetMetaTests
             Slug = "slug",
             LanguageId = 1,
             IsPublished = true,
+            ShowInFeed = true,
             PublishedAt = DateTime.UtcNow
         };
 
@@ -120,5 +122,50 @@ public class GetMetaTests
 
         actual.Should().HaveCount(1);
         actual.Select(x => x.Url).Should().Contain($"/{post.Id}/{post.Slug}");
+    }
+
+    [Fact]
+    public async Task LocalesMetaList_ShouldListLocales()
+    {
+        var post1 = new Post
+        {
+            Title = "title1",
+            Slug = "slug1",
+            LanguageId = 1,
+            IsPublished = true,
+            ShowInFeed = true,
+            PublishedAt = DateTime.UtcNow
+        };
+
+        var post2 = new Post
+        {
+            Title = "title2",
+            Slug = "slug2",
+            LanguageId = 1,
+            IsPublished = true,
+            ShowInFeed = true,
+            PublishedAt = DateTime.UtcNow
+        };
+
+        var post3 = new Post
+        {
+            Title = "title3",
+            Slug = "slug3",
+            LanguageId = 2,
+            IsPublished = true,
+            ShowInFeed = true,
+            PublishedAt = DateTime.UtcNow
+        };
+
+        await _app.AddAsync(post1);
+        await _app.AddAsync(post2);
+        await _app.AddAsync(post3);
+
+        var command = new LocaleMetaList.Query();
+
+        var actual = await _app.SendAsync(command);
+
+        actual.Should().HaveCount(2);
+        actual.Select(x => x.Ln).Should().Contain("lt", "by");
     }
 }
