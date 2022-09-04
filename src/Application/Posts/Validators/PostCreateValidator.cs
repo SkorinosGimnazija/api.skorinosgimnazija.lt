268568404296b8f3f3f1a2e79a28875e44839293
eXmaS <76765030+eXmaS@users.noreply.github.com>
2022-09-04 23:54:55 +0300

    image size validation

--- src/Application/Posts/Validators/PostCreateValidator.cs
+++ src/Application/Posts/Validators/PostCreateValidator.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Http;
 
 public class PostCreateValidator : AbstractValidator<PostCreateDto>
 {
+    private const long MaxImageSize = 10485760;
+
     public PostCreateValidator()
     {
         RuleFor(x => x.PublishedAt).NotEmpty();
@@ -13,6 +15,8 @@ public class PostCreateValidator : AbstractValidator<PostCreateDto>
         RuleFor(x => x.Meta).MaximumLength(256);
         RuleFor(x => x.Title).NotEmpty().MaximumLength(256);
         RuleFor(x => x.NewFiles).Must(BeUnique).WithMessage("File names must be unique");
+        RuleFor(x => x.NewImages).Must(BeUnderSizeLimit).WithMessage("Image size is too large");
+        RuleFor(x => x.NewFeaturedImage).Must(BeUnderSizeLimit).WithMessage("Image size is too large");
         RuleFor(x => x.Slug)
             .NotEmpty()
             .MaximumLength(256)
@@ -20,6 +24,26 @@ public class PostCreateValidator : AbstractValidator<PostCreateDto>
             {
                 RuleFor(x => x.Slug).Must(x => !x.Contains('/')).WithMessage("Forbidden char '/'");
             });
+    }
+
+    private static bool BeUnderSizeLimit(PostCreateDto post, IFormFileCollection? images)
+    {
+        if (images is null)
+        {
+            return true;
+        }
+
+        return images.All(x => x.Length < MaxImageSize);
+    }
+
+    private static bool BeUnderSizeLimit(PostCreateDto post, IFormFile? image)
+    {
+        if (image is null)
+        {
+            return true;
+        }
+
+        return image.Length < MaxImageSize;
     }
 
     private static bool BeUnique(PostCreateDto post, IFormFileCollection? files)
