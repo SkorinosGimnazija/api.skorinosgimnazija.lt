7362d593f5eb4409a23707a32a33394a1755c082
eXmaS <76765030+eXmaS@users.noreply.github.com>
2022-11-22 09:00:48 +0200

    revalidation error logging

--- src/Infrastructure/Revalidation/RevalidationService.cs
+++ src/Infrastructure/Revalidation/RevalidationService.cs
@@ -2,56 +2,78 @@
 
 using System.Net.Http.Json;
 using Domain.Options;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 public sealed class RevalidationService : IRevalidationService
 {
     private readonly IHttpClientFactory _httpClientFactory;
+    private readonly ILogger<RevalidationService> _logger;
     private readonly string _token;
     private readonly string _url;
 
     public RevalidationService(
         IHttpClientFactory httpClientFactory,
-        IOptions<PostRevalidationOptions> revalidationOptions)
+        IOptions<PostRevalidationOptions> revalidationOptions,
+        ILogger<RevalidationService> logger)
     {
         _httpClientFactory = httpClientFactory;
+        _logger = logger;
         _token = revalidationOptions.Value.Token;
         _url = revalidationOptions.Value.Url;
     }
 
     public async Task<bool> RevalidateAsync(string language, string slug, int postId)
     {
-        var message = new HttpRequestMessage(HttpMethod.Post, _url)
+        try
         {
-            Content = new FormUrlEncodedContent(new Dictionary<string, string>
+            var message = new HttpRequestMessage(HttpMethod.Post, _url)
             {
-                { "token", _token },
-                { "lang", language },
-                { "postId", postId.ToString() },
-                { "slug", slug }
-            })
-        };
+                Content = new FormUrlEncodedContent(new Dictionary<string, string>
+                {
+                    { "token", _token },
+                    { "lang", language },
+                    { "postId", postId.ToString() },
+                    { "slug", slug }
+                })
+            };
 
-        var request = await _httpClientFactory.CreateClient().SendAsync(message);
-        var response = await request.Content.ReadFromJsonAsync<RevalidationResponse>();
+            var request = await _httpClientFactory.CreateClient().SendAsync(message);
+            var response = await request.Content.ReadFromJsonAsync<RevalidationResponse>();
 
-        return response?.Success == true;
+            return response?.Success == true;
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(0, e, "Revalidation failed");
+        }
+
+        return false;
     }
 
     public async Task<bool> RevalidateAsync(string language)
     {
-        var message = new HttpRequestMessage(HttpMethod.Post, _url)
+        try
         {
-            Content = new FormUrlEncodedContent(new Dictionary<string, string>
+            var message = new HttpRequestMessage(HttpMethod.Post, _url)
             {
-                { "token", _token },
-                { "lang", language }
-            })
-        };
+                Content = new FormUrlEncodedContent(new Dictionary<string, string>
+                {
+                    { "token", _token },
+                    { "lang", language }
+                })
+            };
 
-        var request = await _httpClientFactory.CreateClient().SendAsync(message);
-        var response = await request.Content.ReadFromJsonAsync<RevalidationResponse>();
+            var request = await _httpClientFactory.CreateClient().SendAsync(message);
+            var response = await request.Content.ReadFromJsonAsync<RevalidationResponse>();
 
-        return response?.Success == true;
+            return response?.Success == true;
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(0, e, "Revalidation failed");
+        }
+
+        return false;
     }
 }
