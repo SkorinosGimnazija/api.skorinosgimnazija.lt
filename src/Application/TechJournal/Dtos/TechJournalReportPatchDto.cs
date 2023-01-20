93446f0d78529cde9d8fe0ab2a2705d51fb7e3aa
eXmaS <76765030+eXmaS@users.noreply.github.com>
2023-01-20 14:27:32 +0200

    tech journal add null status

--- src/Application/TechJournal/Dtos/TechJournalReportPatchDto.cs
+++ src/Application/TechJournal/Dtos/TechJournalReportPatchDto.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 public record TechJournalReportPatchDto
 {
-    public bool IsFixed { get; init; }
+    public bool? IsFixed { get; init; }
 
     public string? Notes { get; init; }
 }
