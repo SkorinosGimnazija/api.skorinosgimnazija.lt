6f755ff7d1beb9fb58e6295d5f6780851c2fa36f
eXmaS <76765030+eXmaS@users.noreply.github.com>
2026-08-17 15:10:07 +0300

    Update CaptchaService.cs

--- API/Services/Captcha/CaptchaService.cs
+++ API/Services/Captcha/CaptchaService.cs
@@ -7,21 +7,20 @@ public class CaptchaService(
     IHttpClientFactory httpClientFactory,
     IOptions<CaptchaOptions> captchaOptions)
 {
-    private const string BaseUrl = "https://www.google.com/recaptcha/api/siteverify";
+    private const string Url = "https://www.google.com/recaptcha/api/siteverify";
 
     public async Task<bool> ValidateAsync(string token)
     {
-        using var client = httpClientFactory.CreateClient();
-
         using var content = new FormUrlEncodedContent([
             // new("remoteip", ip),
             new("secret", captchaOptions.Value.Secret),
             new("response", token)
         ]);
 
-        var request = await client.PostAsync(BaseUrl, content);
-        var response = await request.Content.ReadFromJsonAsync<CaptchaResponse>();
+        var client = httpClientFactory.CreateClient();
+        using var response = await client.PostAsync(Url, content);
+        var data = await response.Content.ReadFromJsonAsync<CaptchaResponse>();
 
-        return response is { Success: true, Score: > 0.5 };
+        return data is { Success: true, Score: > 0.5 };
     }
 }
